294041fd6ec31b6131a22a120f68e564ecccc495
anuwuth <anuwuth.ch@gmail.com>
2026-03-02 09:47:12 +0700

    aaa

---
+++ src/user/user.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get } from '@nestjs/common';
+import { UserService } from './user.service';
+
+@Controller('users/test')
+export class UserController {
+  constructor(private readonly UserService: UserService) {}
+
+  @Get()
+  test(): string[] {
+    return this.UserService.test();
+  }
+
+  
+}
