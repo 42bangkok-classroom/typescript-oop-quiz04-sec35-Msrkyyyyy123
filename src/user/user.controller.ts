2233d08604fb4b091a00482cf08c3b32c40d34a9
anuwuth <anuwuth.ch@gmail.com>
2026-03-22 15:34:34 +0700

    bbb

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,11 +2,11 @@ import { Controller, Get } from '@nestjs/common';
 import { UserService } from './user.service';
 import { User } from './user.interface';
 
-@Controller('users/test')
+@Controller('users')
 export class UserController {
   constructor(private readonly UserService: UserService) {}
 
-  @Get()
+  @Get('test')
   test(): User[] {
     return this.UserService.test();
   }
